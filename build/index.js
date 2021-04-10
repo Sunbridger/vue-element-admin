e565f32c0aa61988e9d2e53f0a1ed189dc54d2c2
Sunbridger <739272884@qq.com>
2021-04-10 22:20:03 +0800

    feat : change path

--- build/index.js
+++ build/index.js
@@ -18,7 +18,7 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
 
   app.use(
     publicPath,
-    serveStatic('./dist', {
+    serveStatic('https://sunbridger.github.io/vue-element-admin', {
       index: ['index.html', '/']
     })
   )
